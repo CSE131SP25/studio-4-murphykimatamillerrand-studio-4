39ddbd69b1e40ce2ff71f1e3d5c77db6dadc0430
lewisrand <lewisrand@localhost>
2025-02-19 15:28:19 -0600

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,41 @@
 package studio4;
 
 import java.awt.Color;
-
+import java.awt.Font;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+
+		StdDraw.setCanvasSize(1250, 750);
+		StdDraw.setPenColor(Color.BLACK);
+
+		StdDraw.setPenColor(new Color(0, 255, 255));
+		StdDraw.filledCircle(0.5, 0.5, 0.85);
+
+		StdDraw.setPenColor(new Color(137, 207, 240));
+		StdDraw.filledCircle(0.5, 0.5, 0.75);
+
+		StdDraw.setPenColor(new Color(115, 147, 179));
+		StdDraw.filledCircle(0.5, 0.5, 0.65);
+
+		StdDraw.setPenColor(new Color(100, 149, 237));
+		StdDraw.filledCircle(0.5, 0.5, 0.55);
+
+		StdDraw.setPenColor(new Color(0, 71, 171));
+		StdDraw.filledCircle(0.5, 0.5, 0.45);
+
+		StdDraw.setPenColor(new Color(111, 143, 175));
+		StdDraw.filledCircle(0.5, 0.5, 0.35);
+
+		StdDraw.setPenColor(new Color(20, 52, 64));
+		StdDraw.filledCircle(0.5, 0.5, 0.25);
+
+		Font font = new Font("Times New Roman", Font.BOLD, 35);
+		StdDraw.setFont(font);
+
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.text(0.5, 0.5, "String countryName = in.nextString();");
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -18,7 +19,25 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		Scanner in = new Scanner(f); // making Scanner with a File
+
+		String shape = in.next();
+		int blueValue = in.nextInt();
+		int redValue = in.nextInt();
+		int greenValue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double dOne = in.nextDouble();
+		double dTwo = in.nextDouble();
+		double dThree = in.nextDouble();
+		double dFour = in.nextDouble();
+
+		if (isFilled != true) {
+			StdDraw.setPenColor(new Color(blueValue, redValue, greenValue));
+			StdDraw.rectangle(dOne, dTwo, dThree, dFour);
+		} else {
+			StdDraw.setPenColor(new Color(blueValue, redValue, greenValue));
+			StdDraw.filledRectangle(dOne, dTwo, dThree, dFour);
+		}
+
 	}
 }
